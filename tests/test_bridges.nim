dd3bc67c1e8b5c4439a3c8740d4d8ffd8193c979
Luca <nilnilnilnil@protonmail.com>
2021-10-25 20:58:19 +0300

    get test

--- tests/test_bridges.nim
+++ tests/test_bridges.nim
@@ -12,6 +12,17 @@ proc isValidObfs4(obfs4: string): bool =
 
     else:
       return true
+
+proc isValidSnowflake(snowflake: string): bool =
+  let s = snowflake.splitWhitespace()
+  if s.len == 2:
+    
+    if (not s[0].match(re"(\d+\.){3}(\d+):\d+")) or
+    (not s[1].match(re".+")):
+      return false
+    
+    else:
+      return true
     
 block:
   assert isValidObfs4("obfs4 122.148.194.24:993 07784768F54CF66F9D588E19E8EE3B0FA702711B cert=m3jPGnUyZMWHT9Riioob95s1czvGs3HiZ64GIT3QbH/AZDVlF/YEXu/OtyYZ1eObKnTjcg iat-mode=0") == true
@@ -19,3 +30,7 @@ block:
   assert isValidObfs4("obfs4 122.148.194.24:993 cert=m3jPGnUyZMWHT9Riioob95s1czvGs3HiZ64GIT3QbH/AZDVlF/YEXu/OtyYZ1eObKnTjcg iat-mode=0") == false
   assert isValidObfs4("obfs4 122.148.194.24:993 07784768F54CF66F9D588E19E8EE3B0FA702711B cert= iat-mode=0") == false
   assert isValidObfs4("obfs4 122.148.194.24:993 07784768F54CF66F9D588E19E8EE3B0FA702711B cert=m3jPGnUyZMWHT9Riioob95s1czvGs3HiZ64GIT3QbH/AZDVlF/YEXu/OtyYZ1eObKnTjcg iat-mode=19") == false
+  
+block:
+  let strs = "#Bridge snowflake 192.0.2.3:1 2B280B23E1107BB62ABFC40DDCC8824814F80A72".splitWhitespace(2)
+  assert isValidSnowflake(strs[2]) == true
