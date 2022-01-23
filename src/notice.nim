f25c3f9701defacd0be373344fe32443cf0c992a
Luca <nilnilnilnil@protonmail.com>
2022-01-23 05:17:14 +0100

    add notice

---
+++ src/notice.nim
@@ -0,0 +1,38 @@
+from types import State
+
+type
+  Notice = ref object of RootObj
+    state: State
+    msg: string
+
+  Notifies* = ref object
+    notice: seq[Notice]
+
+method getState*(n: Notice): State {.base.} =
+  n.state
+
+method getMsg*(n: Notice): string {.base.} =
+  n.msg
+
+method len*(n: Notifies): int {.base.} =
+  n.notice.len
+
+proc new*(): Notifies =
+  Notifies()
+
+proc add*(n: var Notifies, state: State, msg: string) =
+  if msg.len == 0: return
+  let notice = Notice(state: state, msg: msg)
+  n.notice.add notice
+
+iterator items*(n: Notifies): tuple[i: int, n: Notice] {.inline.} =
+  var i: int
+  while i < n.len:
+    yield (i, n.notice[i])
+    inc i
+
+iterator body*(n: Notifies): Notice {.inline.} =
+  var i: int
+  while i < n.len:
+    yield n.notice[i]
+    inc i
