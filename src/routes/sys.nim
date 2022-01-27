7ab4669627f5d69e2b8b7f425ad38c2025a0c129
Luca <nilnilnilnil@protonmail.com>
2022-01-27 15:40:36 +0100

    Update sys router

--- src/routes/sys.nim
+++ src/routes/sys.nim
@@ -1,6 +1,6 @@
 import jester
 import ../ views / [temp, sys]
-import ".." / [types]
+import ".." / [ types, notice ]
 import ".." / lib / sys as libsys 
 import ".." / lib / session
 
@@ -8,9 +8,9 @@ export sys
 
 proc routingSys*(cfg: Config) =
   router sys:
-    let tabForSys = Menu(
-      text: @["Password", "Erase logs", "Update"],
-      anker: @["/sys/passwd", "/sys/eraselogs", "/sys/update"]
+    let tabForSys = Tabs(
+      texts: @["Password", "Erase logs", "Update"],
+      links: @["/sys/passwd", "/sys/eraselogs", "/sys/update"]
     )
 
     get "/sys":
@@ -20,28 +20,33 @@ proc routingSys*(cfg: Config) =
     post "/sys":
       let user = await getUser(request)
       if user.isLoggedIn:
-        if request.formData["postType"].body == "chgPasswd":
+        case request.formData["postType"].body
+        of "chgPasswd":
           let code = await changePasswd(request.formData["crPassword"].body, request.formData["newPassword"].body, request.formData["re_newPassword"].body)
           if code:
             redirect("/login")
           else:
             redirect("/login")
-        elif request.formData["postType"].body == "eraseLogs":
+        of "eraseLogs":
           let erased = await eraseLogs()
+          var notifies: Notifies = new()
           if erased == success:
-            resp renderNode(renderLogs(), request, cfg, user.uname, "", tabForSys, Notify(status: success, msg: "Complete erased logs"))
+            notifies.add success, "Complete erase logs"
+            resp renderNode(renderLogs(), request, user.uname, "", tabForSys, notifies)
+
           elif erased == failure:
-            resp renderNode(renderLogs(), request, cfg, user.uname, "", tabForSys, Notify(status: failure, msg: "Failure erased logs"))
+            notifies.add failure, "Failure erase logs"
+            resp renderNode(renderLogs(), request, user.uname, "", tabForSys, notifies)
 
     get "/sys/passwd":
       let user = await getUser(request)
       if user.isLoggedIn:
         # resp renderNode(renderCard("Change Passwd", renderPasswd()), request, cfg, tabForSys)
-        resp renderNode(renderChangePasswd(), request, cfg, user.uname, "", tabForSys)
+        resp renderNode(renderChangePasswd(), request, user.uname, "", tabForSys)
       redirect "/login"
     
     get "/sys/eraselogs":
       let user = await getUser(request)
       if user.isLoggedIn:
-        resp renderNode(renderLogs(), request, cfg, user.uname, "", tabForSys)
+        resp renderNode(renderLogs(), request, user.uname, "", tabForSys)
       redirect "/login"
