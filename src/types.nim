bdbc9a3ef56e59e6925f8c0932b84c6942ee1b3e
Luca <nilnilnilnil@protonmail.com>
2022-02-24 21:27:58 +0100

    delete unused object

--- src/types.nim
+++ src/types.nim
@@ -1,26 +1,9 @@
 import std / [ uri ]
 import std / [ nativesockets ]
+import lib / hostap
+import lib / sys / [ iface ]
 
 type
-  NotifyBox* = ref object
-    token*: string
-    path*: string
-
-  Spwd* {.importc: "struct spwd", header: "<shadow.h>".} = ptr object
-    name* {.importc: "sp_namp".}: cstring
-    passwd* {.importc: "sp_pwdp".}: cstring
-    sp_lstchg {.importc: "sp_lstchg".}: clong
-    min {.importc: "sp_min".}: clong
-    max {.importc: "sp_max".}: clong
-    warn {.importc: "sp_warn".}: clong
-    inact {.importc: "sp_inact".}: clong
-    expire {.importc: "sp_expire".}: clong
-    flag {.importc: "sp_flag".}: culong
-
-  Protocol* = enum
-    GET = "get",
-    POST = "post"
-
   ActivateObfs4Kind* {.pure.} = enum
     all, online, select
 
@@ -55,30 +38,6 @@ type
     deactive
     online
     offline
-    
-  HostApConf* = ref object
-    isActive*: bool
-    iface*: IfaceKind
-    ssid*: string
-    password*: string
-    band*: string
-    channel*: string
-    isHidden*: bool 
-    power*: string
-  
-  Query* = object
-    iface*: IfaceKind
-    withCaptive*: bool
-  
-  IfaceKind* = enum
-    unkwnIface = "unkwnIface"
-    eth0 = "eth0"
-    eth1 = "eth1"
-    wlan0 = "wlan0"
-    wlan1 = "wlan1"
-    ppp0 = "ppp0"
-    usb0 = "usb0"
-    tun0 = "tun0"
     
   Wifi* = object of RootObj
     bssid*: string
@@ -118,11 +77,3 @@ type
     snowflake*: bool
     
   ConnectedDevs* = seq[tuple[macaddr, ipaddr, signal: string]]
-
-  ActiveIfaceList* = tuple
-    input, output: IfaceKind
-    hasVpn: bool
-  # NetInterfaces* = ref object
-  #   kind*: NetInterKind
-  #   status*: Status
-    #eth0*, eth1*, wlan0*, wlan1*, pop0*, usb0*, tun0*: Status
