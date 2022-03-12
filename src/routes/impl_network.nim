0445ac41d2d6c1c6a1cf15345482b987a65a3d89
Luca <nilnilnilnil@protonmail.com>
2022-03-12 04:34:47 +0100

    network rewrite

---
+++ src/routes/impl_network.nim
@@ -0,0 +1,27 @@
+import std / [ os ]
+import tabs 
+import ".." / [ notice, types ]
+import ".." / lib / [ sys, session, hostap ]
+
+func netTab*(): Tabs =
+  var tab = Tab.new
+  tab.add("Bridges", "/net" / "bridges")
+  tab.add("Interfaces", "/net" / "interfaces")
+  tab.add("Wireless", "/net" / "wireless")
+
+template respNetworkManager*(wifiList: WifiList, curNet: tuple[ssid, ipAddr: string]) =
+  resp renderNode(renderWifiConfig(iface, withCaptive, wifiList, curNet), request, request.getUserName, "Network management", netTab())
+
+template respNetworkManager*(wifiList: WifiList, curNet: tuple[ssid, ipAddr: string], n: Notifies) =
+  resp renderNode(renderWifiConfig(iface, withCaptive, wifiList, curNet), request, request.getUserName, netTab(), n)
+
+template respBridges*() =
+  let bridgesSta = await getBridgeStatuses()
+  resp renderNode(renderBridgesPage(bridgesSta), request, request.getUserName, "Bridges", netTab())
+
+template respBridges*(n: Notifies) =
+  let bridgesSta = await getBridgeStatuses()
+  resp renderNode(renderBridgesPage(bridgesSta), request, request.getUserName, "Bridges", netTab(), n)
+
+template respMaintenance*() =
+  resp renderNode(renderClosed(), request, request.getUserName, "Under maintenance", netTab())
--- src/routes/network.nim
+++ src/routes/network.nim
@@ -1,45 +1,23 @@
-import std / [ strutils, os ]
+import std / [ strutils ]
 import jester 
-import tabs
+import impl_network
 import ../ views / [ temp, network ]
-import ".." / [ types, query, utils, notice ]
-import ".." / lib / [ sys, torbox, session, hostAp, fallbacks, wifiScanner, wirelessManager ]
+import ".." / [ types, notice ]
+import ".." / lib / [ sys, torbox, session, hostap ]
 import ../ lib / tor / tor
-import network / wireless
+import network / [ wireless, impl_wireless ]
 
-export network, wireless
+export network, impl_network, wireless, impl_wireless
 
-var net_tab* = Tab.new
-net_tab.add("Bridges", "/net" / "bridges")
-net_tab.add("Interfaces", "/net" / "interfaces")
-net_tab.add("Wireless", "/net" / "wireless")
-
-template respNetworkManager*(wifiList: WifiList, curNet: tuple[ssid, ipAddr: string]) =
-  resp renderNode(renderWifiConfig(iface, withCaptive, wifiList, curNet), request, request.getUserName, "Network management", net_tab)
-
-template respNetworkManager*(wifiList: WifiList, curNet: tuple[ssid, ipAddr: string], n: Notifies) =
-  resp renderNode(renderWifiConfig(iface, withCaptive, wifiList, curNet), request, request.getUserName, tab, n)
-
-template respBridges*() =
-  let bridgesSta = await getBridgeStatuses()
-  resp renderNode(renderBridgesPage(bridgesSta), request, request.getUserName, "Bridges", net_tab)
-
-template respBridges*(n: Notifies) =
-  let bridgesSta = await getBridgeStatuses()
-  resp renderNode(renderBridgesPage(bridgesSta), request, request.getUserName, "Bridges", net_tab, n)
-
-template respMaintenance*() =
-  resp renderNode(renderClosed(), request, request.getUserName, "Under maintenance", net_tab)
-
-proc routingNet*(sysInfo: SystemInfo) =
-  routerWireless(sysInfo)
+# routerwireless()
+proc routingNet*() =
 
   router network:
 
-    extend wireless, ""
+    # extend wireless, ""
+    # let tab = netTab()
 
     var net: Network = new Network
-
     get "/bridges":
       loggedIn:
         respBridges()
@@ -52,63 +30,64 @@ proc routingNet*(sysInfo: SystemInfo) =
     get "/interfaces/set/?":
       loggedIn:
         respMaintenance()
-        let
-          query = initQuery(request.params)
-          iface = query.iface
+        # let
+        #   query = initQuery(request.params)
+        #   iface = query.iface
 
-        var clientWln, clientEth: IfaceKind
-        # let query = initQuery(params(request))
+        # var clientWln, clientEth: IfaceKind
+        # # let query = initQuery(params(request))
         
-        if not ifaceExists(iface):
-          redirect "interfaces"
+        # if not ifaceExists(iface):
+        #   redirect "interfaces"
 
-        case iface
-        of wlan0, wlan1:
+        # case iface
+        # of wlan0, wlan1:
 
-          case iface
-          of wlan0:
-            clientWln = wlan1
-            clientEth = eth0
+        #   case iface
+        #   of wlan0:
+        #     clientWln = wlan1
+        #     clientEth = eth0
 
-          of wlan1:
-            clientWln = wlan0
-            clientEth = eth0
+        #   of wlan1:
+        #     clientWln = wlan0
+        #     clientEth = eth0
 
-          else: redirect "interfaces"
+        #   else: redirect "interfaces"
           
-          hostapdFallbackKomplex(clientWln, clientEth)
-          editTorrc(iface, clientWln, clientEth)
-          restartDhcpServer()
+        #   hostapdFallbackKomplex(clientWln, clientEth)
+        #   editTorrc(iface, clientWln, clientEth)
+        #   restartDhcpServer()
 
-          # net.scanned = true
-          # if wifiScanResult.code:
-          # resp renderNode(renderWifiConfig(@"interface", wifiScanResult, currentNetwork), request, cfg, tab)
-          if query.withCaptive:
-            redirect "interfaces/join/?iface=" & $iface & "&captive=1"
+        #   # net.scanned = true
+        #   # if wifiScanResult.code:
+        #   # resp renderNode(renderWifiConfig(@"interface", wifiScanResult, currentNetwork), request, cfg, tab)
+        #   if query.withCaptive:
+        #     redirect "interfaces/join/?iface=" & $iface & "&captive=1"
 
-          redirect "interfaces/join/?iface=" & $iface
-        else: redirect "interfaces"
+        #   redirect "interfaces/join/?iface=" & $iface
+        # else: redirect "interfaces"
 
     get "/interfaces/join/?":
       # let user = await getUser(request)
       # if user.isLoggedIn:
       loggedIn:
-        let
-          query = initQuery(request.params)
-          iface = query.iface
-          withCaptive = query.withCaptive
+        respMaintenance()
+        # let
+        #   query = initQuery(request.params)
+        #   iface = query.iface
+        #   withCaptive = query.withCaptive
 
-        case iface
-        of wlan0, wlan1:
-          var wpa = await newWpa(iface)
-          let
-            wifiScanResult = await networkList(wpa)
-            currentNetwork = await currentNetwork(wpa.wlan)
-          net = wpa
-          respNetworkManager(wifiScanResult, currentNetwork)
+        # case iface
+        # of wlan0, wlan1:
+        #   var wpa = await newWpa(iface)
+        #   let
+        #     wifiScanResult = await networkList(wpa)
+        #     currentNetwork = await currentNetwork(wpa.wlan)
+        #   net = wpa
+        #   respNetworkManager(wifiScanResult, currentNetwork)
         
-        else:
-          redirect "interfaces"
+        # else:
+        #   redirect "interfaces"
 
     post "/apctl":
       loggedIn:
@@ -131,61 +110,62 @@ proc routingNet*(sysInfo: SystemInfo) =
 
     post "/interfaces/join/@wlan":
       loggedIn:
-        var clientWln, clientEth: IfaceKind
-        let
-          iface = parseIface(@"wlan")
-          captive = request.formData.getOrDefault("captive").body
-          withCaptive = if captive == "1": true else: false
+        respMaintenance()
+        # var clientWln, clientEth: IfaceKind
+        # let
+        #   iface = parseIface(@"wlan")
+        #   captive = request.formData.getOrDefault("captive").body
+        #   withCaptive = if captive == "1": true else: false
 
-        if not ifaceExists(iface):
-          redirect "interfaces"
+        # if not ifaceExists(iface):
+        #   redirect "interfaces"
 
-        elif not net.scanned:
-          redirect "interfaces"
+        # elif not net.scanned:
+        #   redirect "interfaces"
 
-        case iface
-        of wlan0, wlan1:
+        # case iface
+        # of wlan0, wlan1:
 
-          case iface
-          of wlan0:
-            clientWln = wlan1
-            clientEth = eth0
+        #   case iface
+        #   of wlan0:
+        #     clientWln = wlan1
+        #     clientEth = eth0
 
-          of wlan1:
-            clientWln = wlan0
-            clientEth = eth0
+        #   of wlan1:
+        #     clientWln = wlan0
+        #     clientEth = eth0
 
-          else: redirect "interfaces"
+        #   else: redirect "interfaces"
 
-          let
-            essid = request.formData.getOrDefault("essid").body
-            bssid = request.formData.getOrDefault("bssid").body
-            password = request.formData.getOrDefault("password").body
-            # sec = request.formData.getOrDefault("security").body
-            cloak = request.formData.getOrDefault("cloak").body
-            ess = request.formData.getOrDefault("ess").body
+        #   let
+        #     essid = request.formData.getOrDefault("essid").body
+        #     bssid = request.formData.getOrDefault("bssid").body
+        #     password = request.formData.getOrDefault("password").body
+        #     # sec = request.formData.getOrDefault("security").body
+        #     cloak = request.formData.getOrDefault("cloak").body
+        #     ess = request.formData.getOrDefault("ess").body
 
-          net.isHidden = if cloak == "0": true else: false
-          net.isEss = if ess == "0": true else: false
+        #   net.isHidden = if cloak == "0": true else: false
+        #   net.isEss = if ess == "0": true else: false
 
-          if not net.isEss:
-            if password.len == 0:
-              redirect "interfaces"
+        #   if not net.isEss:
+        #     if password.len == 0:
+        #       redirect "interfaces"
 
-          if (essid.len != 0) or (bssid.len != 0):
-            let con = await connect(net, (essid: essid, bssid: bssid, password: password))
+        #   if (essid.len != 0) or (bssid.len != 0):
+        #     let con = await connect(net, (essid: essid, bssid: bssid, password: password))
 
-            if con.code:
-              if withCaptive:
-                setCaptive(iface, clientWln, clientEth)
-              setInterface(iface, clientWln, clientEth)
-              saveIptables()
-              redirect "interfaces"
-            net = new Network
+        #     if con.code:
+        #       if withCaptive:
+        #         setCaptive(iface, clientWln, clientEth)
+        #       setInterface(iface, clientWln, clientEth)
+        #       saveIptables()
+        #       redirect "interfaces"
+        #     net = new Network
 
-          redirect "interfaces"
-        else:
-          redirect "interfaces"
+        #   redirect "interfaces"
+        # else:
+        #   redirect "interfaces"
         # newConnect()
       
     post "/bridges":
