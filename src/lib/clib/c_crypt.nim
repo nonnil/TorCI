790438ab4281ee3144e59d91d264df68df2fae9b
Luca <nilnilnilnil@protonmail.com>
2022-03-25 15:04:41 +0100

    implement crypt wrapper for string type

--- src/lib/clib/c_crypt.nim
+++ src/lib/clib/c_crypt.nim
@@ -1,3 +1,7 @@
 {.passL: "-lcrypt".}
 
 proc crypt*(key, salt: cstring): cstring {.importc: "crypt", header: "<crypt.h>".}
+
+proc crypt*(key, salt: string): string =
+  let crypt = crypt(cstring(key), cstring(salt))
+  $crypt
