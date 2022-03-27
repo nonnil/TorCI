21576828d6c48ebb1552fb80cc133f4eea02f855
Luca <nilnilnilnil@protonmail.com>
2022-03-27 01:53:37 +0100

    crypt from libc test

---
+++ tests/test_c_crypt.nim
@@ -0,0 +1,13 @@
+import std / unittest, system
+import strformat
+import ../ src / lib / clib / c_crypt
+
+suite "Encrypt password":
+  test "do crypt":
+    const
+      shadow = "$6$FRuqFx.gDQotf$xph8gaXXM2D1Y8WMYPfUgLUlQivlc/cAZtB2x.xZbIACrlfqnZtgeVAGcVwvV/embpKisdSKSlVkhrEVR0H3X."
+      salt = "FRuqFx.gDQotf"
+
+    check:
+      shadow == $crypt("nim", fmt"$6${cstring salt}")
+      shadow == crypt("nim", fmt"$6${salt}")
