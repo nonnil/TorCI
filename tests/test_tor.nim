6ea2423a8b6229f90ff58829a246cf878d024050
Luca <nillability@protonmail.com>
2022-05-09 17:24:18 +0900

    Update tests for tor

--- tests/test_tor.nim
+++ tests/test_tor.nim
@@ -3,35 +3,48 @@ import std / [
   options,
   nativesockets, asyncdispatch
 ]
-import results, resultsutils, optionsutils
+import results, resultsutils
 import karax / vdom as kvdom
 import ../ src / lib / sys / service
 import ../ src / lib / tor / tor {.all.}
 import ../ src / lib / tor / bridges {.all.}
 import ../ src / lib / tor / vdom
 
-suite "Tor tests":
-  test "Test some methods of Tor":
-    privateAccess(TorInfo)
-    privateAccess(TorStatus)
-
+suite "Tor":
+  proc skip() =
     if not waitFor isActiveService("tor"):
       skip()
 
-    var torStatus: TorStatus
+  test "TorInfo object":
+    skip()
+    var torInfo: TorInfo
 
-    match waitFor checkTor("127.0.0.1", 9050.Port):
-      Ok(status): torStatus = status
-      Err(msg): styledEcho(fgRed, "[Error] ", fgWhite, msg); skip()
+    match waitFor getTorInfo("127.0.0.1", 9050.Port):
+      Ok(ret): torInfo = ret
+      Err(msg): fail
 
     check:
-      torStatus.isTor
-      withSome torStatus.exitIp:
-        some exitIp:
-          styledEcho(fgGreen, "[Tor is working]")
-          styledEcho(fgGreen, "[Exit node ip address] ", fgWhite, exitIp)
-          true
-        none: false
+      torInfo.isTor
+
+  # test "Test some methods of Tor":
+  #   privateAccess(TorInfo)
+  #   privateAccess(TorStatus)
+
+
+  #   var torStatus: TorStatus
+
+  #   match waitFor checkTor("127.0.0.1", 9050.Port):
+  #     Ok(status): torStatus = status
+  #     Err(msg): styledEcho(fgRed, "[Error] ", fgWhite, msg); skip()
+
+  #   check:
+  #     torStatus.isTor
+  #     withSome torStatus.exitIp:
+  #       some exitIp:
+  #         styledEcho(fgGreen, "[Tor is working]")
+  #         styledEcho(fgGreen, "[Exit node ip address] ", fgWhite, exitIp)
+  #         true
+  #       none: false
   
   test "Test vdom rendering with TorInfo":
     privateAccess(TorInfo)
@@ -40,7 +53,7 @@ suite "Tor tests":
     let
       status = TorStatus(
         isTor: true,
-        exitIp: some"1.1.1.1"
+        exitIp: "1.1.1.1"
       )
       bridge = Bridge(
         kind: BridgeKind.obfs4,
@@ -55,4 +68,4 @@ suite "Tor tests":
     let dom = dummy.render()
     # styledEcho(fgGreen, "[VDom] ", fgWhite, $dom)
     check:
-      len($dom) > 0
+      0 < len($dom)
