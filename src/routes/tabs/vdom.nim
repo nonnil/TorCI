1ad870708eb2d7e25ff1390b513bbbbe2cdac168
Luca <nillability@protonmail.com>
2022-05-11 02:14:46 +0900

    Update vdom for Tab obj

--- src/routes/tabs/vdom.nim
+++ src/routes/tabs/vdom.nim
@@ -1,13 +1,13 @@
-import std / [ options, strutils ]
+import std / [ strutils ]
 import karax / [ karaxdsl, vdom ]
 import tab
 
-proc render*(tab: Tab, currentPath: string): VNode =
+func render*(self: Tab, currentPath: string): VNode =
   buildHtml(tdiv(class="sub-menu")):
     ul(class="menu-table"):
-      for i, label, link in tab.items:
-        let class = if currentPath.startsWith(link.get): "menu-item current"
+      for i, v in self.list:
+        let class = if currentPath.startsWith(v.link): "menu-item current"
                     else: "menu-item"
         li(class=class):
-          a(class="menu-link", href=link.get):
-            text label.get
+          a(class="menu-link", href=v.link):
+            text v.label
