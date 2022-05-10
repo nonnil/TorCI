601657d3f5fde08e0451074c6372fb03281993b8
Luca <nillability@protonmail.com>
2022-05-11 02:13:22 +0900

    Update tab.nim

--- src/routes/tabs/tab.nim
+++ src/routes/tabs/tab.nim
@@ -1,11 +1,10 @@
-import std / [ options ]
-# import results
-
 type
   Tab* = ref object
-    tab: seq[TabField]
+    tab: TabList
 
-  TabField = ref object
+  TabList* = seq[TabField]
+
+  TabField* = ref object
     label: string
     link: string
 
@@ -17,23 +16,21 @@ method len*(tab: Tab): int {.base.} =
 method isEmpty*(tab: Tab): bool {.base.} =
   tab.len == 0 
 
-proc getLabel*(tab: Tab, i: int): string =
+proc label*(tab: Tab, i: int): string =
   tab.tab[i].label
 
-proc getLink*(tab: Tab, i: int): string =
+proc link*(tab: Tab, i: int): string =
   tab.tab[i].link
 
-proc `[]`*(tab: Tab, i: int): Option[TabField] =
-  if not tab.tab[i].isNil:
-    return some tab.tab[i]
+proc `[]`*(tab: Tab, i: int): TabField =
+  # if not tab.tab[i].isNil:
+  tab.tab[i]
 
-proc getLabel*(tab: TabField): Option[string] =
-  if tab.label.len > 0:
-    return some tab.label
+proc label*(tab: TabField): string =
+  tab.label
 
-proc getLink*(tab: TabField): Option[string] =
-  if tab.link.len > 0:
-    return some tab.link
+proc link*(tab: TabField): string =
+  tab.link
 
 proc add*(tab: var Tab, label, link: string) =
   let field: TabField = TabField(label: label, link: link)
@@ -43,9 +40,11 @@ proc add*(tab: Tab, label, link: string) =
   let field: TabField = TabField(label: label, link: link)
   tab.tab.add field
 
-iterator items*(tab: Tab): tuple[i: int, label, link: Option[string]] {.inline.} =
-  var i: int
-  while i < tab.len:
-    let f = tab[i]
-    if f.isSome:
-      yield (i, f.get.getLabel, f.get.getLink)
+method list*(self: Tab): TabList {.base.} =
+  self.tab
+
+# iterator items*(tab: Tab): tuple[i: int, label, link: string] {.inline.} =
+#   var i: int
+#   while i < tab.len:
+#     let f = tab[i]
+#     yield (i, f.label, f.link)
