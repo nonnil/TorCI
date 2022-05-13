e4d2a8059120cc206689916d75fa69f7308055fc
Luca <nillability@protonmail.com>
2022-05-14 04:20:01 +0900

    Update test_status.nim

--- tests/test_routes/test_status.nim
+++ tests/test_routes/test_status.nim
@@ -5,20 +5,15 @@ import std / [
 import ../ server / client
 
 suite "route status":
-  # const
-  #   paths = @[ "torinfo", "iface", "systeminfo" ]
-
-  # # start process
-  # let process: Process = start("status")
-
-  # waitFor clientStart("0.0.0.0", 1984.Port, paths)
-
-  # kill(process)
   routerTest "status":
     GET:
-      "/torinfo"
+      "/tor"
+      "/default/tor"
       "/iface"
-      "/systeminfo"
+      "/default/iface"
+      "/sys"
+      "/default/sys"
+      # "/default/sys"
 
     # POST:
     #   "/io": {"tor-request": "renew"}
