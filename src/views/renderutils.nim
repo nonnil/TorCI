4a08d8ac611865e91e350fa8d8fdaa572f706c9c
Luca <nillability@protonmail.com>
2022-05-14 05:03:46 +0900

    Update renderutils.nim

--- src/views/renderutils.nim
+++ src/views/renderutils.nim
@@ -173,3 +173,39 @@ macro render*(title: string, body: untyped): string =
     nc
   )
   n
+
+proc renderError*(e: string): VNode =
+  buildHtml():
+    tdiv(class="content"):
+      tdiv(class="panel-container"):
+        tdiv(class="logo-container"):
+          img(class="logo", src="/images/torbox.png", alt="TorBox")
+        tdiv(class="error-panel"):
+          span(): text e
+          
+proc renderClosed*(): VNode =
+  buildHtml():
+    tdiv(class="warn-panel"):
+      icon "attention", class="warn-icon"
+      tdiv(class="warn-subject"): text "Sorry..."
+      tdiv(class="warn-description"):
+        text "This feature is currently closed as it is under development and can cause bugs"
+
+proc renderPanel*(v: VNode): VNode =
+  buildHtml(tdiv(class="main-panel")):
+    v
+
+proc renderFlat*(v: VNode, title: string = ""): string =
+  let ret = buildHtml(html(lang="en")):
+    renderHead(cfg, title)
+    body:
+      v
+  result = doctype & $ret
+
+proc renderFlat*(v: VNode, title: string = "", notifies: Notifies): string =
+  let ret = buildHtml(html(lang="en")):
+    renderHead(cfg, title)
+    body:
+      notifies.render
+      v
+  result = doctype & $ret
