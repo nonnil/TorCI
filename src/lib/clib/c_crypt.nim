6ec243380ab8d31f343978de3ab9710e097afa9c
Luca <nillability@protonmail.com>
2022-05-15 00:54:54 +0900

    update crypt from libc

--- src/lib/clib/c_crypt.nim
+++ src/lib/clib/c_crypt.nim
@@ -1,7 +1,5 @@
 {.passL: "-lcrypt".}
-
 proc crypt*(key, salt: cstring): cstring {.importc: "crypt", header: "<crypt.h>".}
-
 proc crypt*(key, salt: string): string =
   let crypt = crypt(cstring(key), cstring(salt))
   $crypt
