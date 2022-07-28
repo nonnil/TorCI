e0ebb065b8913b55882281fcf860da712961d408
Luca <nillability@protonmail.com>
2022-07-28 13:03:22 +0900

    Update test_redis

--- tests/sandbox/tests/test_redis.nim
+++ tests/sandbox/tests/test_redis.nim
@@ -5,16 +5,18 @@ converter toInt(x: int64): int = cast[int](x)
 
 proc main() {.async.} =
   let client = await openAsync(port=7000.Port)
-  # client.startPipelining()
+
   let token = "randomness0"
   let strct = @[
     ("username", "Tor-chan")
   ]
   echo "is nil? ", await client.hGet(token, "username")
+
   let res = await client.setEx(token, 3, "Tor-chan")
   echo "res: ", res
   echo "username before expire: ", await client.get(token)
   echo "sleeping..."
+
   await sleepAsync(300)
   echo "username after expire: ", await client.get(token)
 
@@ -24,15 +26,19 @@ proc main() {.async.} =
 
 proc normal() {.async.} =
   let red = await openAsync(port=7000.Port)
+
   block:
     let res = await red.ping()
     echo res
+    
   block:
     let res = await red.ping()
     echo res
+
   block:
     let res = await red.ping()
     echo res
+
   block:
     let res = await red.ping()
     echo res
@@ -40,18 +46,23 @@ proc normal() {.async.} =
 proc pipe() {.async.} =
   let red = await openAsync(port=7000.Port)
   red.startPipelining()
+
   block:
     let res = await red.ping()
     echo res
+
   block:
     let res = await red.ping()
     echo res
+  
   block:
     let res = await red.ping()
     echo res
+
   block:
     let res = await red.ping()
     echo res
+
   let flushed = await red.flushPipeline()
   echo flushed
 
